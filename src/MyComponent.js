0e035c966afbeffbd18c4f9f145d93a57a41a803
FreeHunter <chronos@kakao.com>
2022-01-17 21:39:51 +0900

    리액터 공부 (p161-162)

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,14 +1,22 @@
-const MyComponent = props => {
-    return (
-        <div>
-            안녕하세요, 제이름은 {props.name}입니다. <br />
-            children 값은 {props.children}
-        </div>
-    );
-};
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-MyComponent.defaultProps = {
-    name: '기본 이름'
-};
+class MyComponent extends Component {
+    static defaultProps = {
+        name: '기본 이름'
+    };
+    render() {
+        const {name, favoriteNumber, children } = this.props;
+        return(
+            <div>
+                안녕하세요, 제 이름은 {name} 입니다. <br />
+                children 값은 {children}
+                입니다.
+                <br />
+                제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+            </div>
+        );
+    }
+}
 
 export default MyComponent;
